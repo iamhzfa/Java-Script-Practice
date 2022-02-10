5cc926e7f798e0c657b16b3629989d6e0ce2b453
iamhzfa <huzefamohammed10@gmail.com>
2022-02-10 18:25:38 +0530

    Done

---
+++ 02_basics/01_variables.js
@@ -0,0 +1,15 @@
+var fullName = " Mohammed Huzefa ";
+
+var courseName = " Dsa ";
+
+var loggedIn = true;
+console.log(loggedIn);
+
+var loggedCount = 39;
+console.log(loggedCount);
+
+var paymentMode;
+console.log(paymentMode);
+
+paymentMode=" Credit Card ";
+console.log(paymentMode);
---
+++ 02_basics/02_const_var.js
@@ -0,0 +1,18 @@
+const uid = "2354abc";
+
+var fullName = 'Mohammed Huzefa';
+var email = 'huzefa@gmail.com';
+var password = "123456";
+var confirmPassword = "123456";
+
+console.log();
+console.log(fullName);
+console.log("Full Name is ", fullName);
+console.log("Full Name is " + fullName);
+
+console.log(`
+    Your Unique Id is : ${uid}
+        Your Full Name is : ${fullName}
+Your Email is     : ${email}
+    Your Password is  : ${password}
+`);
